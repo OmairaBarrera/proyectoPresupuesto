411537016a4b82f53309097df9d1a8af58f81ee3
OmairaBarrera <lomaira.barrera@gmail.com>
2023-03-31 08:12:50 -0500

    feat: :sparkles: Add table function in main.js
    the function return values

--- main.js
+++ main.js
@@ -10,6 +10,18 @@ formularioPresupuesto.addEventListener("submit", (e)=>{
         valor: `${data.valor}`
     });
     console.log(tipoObjeto);
+    tablas();
     formularioPresupuesto.reset();
 });
 
+let tablas = ()=>{
+    document.querySelector('#tabla').innerHTML = ""
+    tipoObjeto.forEach((val,id)=>{
+        document.querySelector('#tablaIngresos').insertAdjacentHTML("beforeend",`    
+            <tr>
+                <td>${val.descripcion}</td> 
+                <td>${val.valor}</td>
+            </tr>
+        `)
+    });
+}
