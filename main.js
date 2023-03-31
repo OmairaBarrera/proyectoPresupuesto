22174b59cd9b12c28555df4ad903246ec451c151
OmairaBarrera <lomaira.barrera@gmail.com>
2023-03-31 08:33:42 -0500

    feat: :white_check_mark: Modify table and form functions
    the function return another values apart income and expenses values.

--- main.js
+++ main.js
@@ -1,23 +1,43 @@
 let formularioPresupuesto = document.querySelector("#formularioPresupuesto");
-let tipoObjeto = [];
+let tipoObjeto = {
+    ingresos:[],
+    egresos:[]
+};
 
 formularioPresupuesto.addEventListener("submit", (e)=>{
     e.preventDefault();
     let data = Object.fromEntries(new FormData(e.target));
-    tipoObjeto.unshift({
-        tipo: `${data.tipo}`,
-        descripcion: `${data.descripcion}`,
-        valor: `${data.valor}`
-    });
-    console.log(tipoObjeto);
+    if(data.tipo == "+"){
+        tipoObjeto["ingresos"].unshift({
+            tipo: `${data.tipo}`,
+            descripcion: `${data.descripcion}`,
+            valor: `${data.valor}`
+        });
+    } else {
+        tipoObjeto["egresos"].unshift({
+            tipo: `${data.tipo}`,
+            descripcion: `${data.descripcion}`,
+            valor: `${data.valor}`
+        });
+    }
     tablas();
+    console.log(tipoObjeto);
     formularioPresupuesto.reset();
 });
 
 let tablas = ()=>{
-    document.querySelector('#tabla').innerHTML = ""
-    tipoObjeto.forEach((val,id)=>{
+    document.querySelector('#tablaIngresos').innerHTML = ""
+    tipoObjeto.ingresos.forEach((val,id)=>{
         document.querySelector('#tablaIngresos').insertAdjacentHTML("beforeend",`    
+            <tr>
+                <td>${val.descripcion}</td> 
+                <td>${val.valor}</td>
+            </tr>
+        `)
+    });
+    document.querySelector('#tablaEgresos').innerHTML = ""
+    tipoObjeto.egresos.forEach((val,id)=>{
+        document.querySelector('#tablaEgresos').insertAdjacentHTML("beforeend",`    
             <tr>
                 <td>${val.descripcion}</td> 
                 <td>${val.valor}</td>
