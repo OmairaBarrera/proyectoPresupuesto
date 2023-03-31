9f19595521c9e78c6f4d2dd108a81d445cff2b8c
OmairaBarrera <lomaira.barrera@gmail.com>
2023-03-31 13:21:00 -0500

    feat: :sparkles: add porcentajes

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let tipoObjeto = {
     egresos:[]
 };
 
+
 formularioPresupuesto.addEventListener("submit", (e)=>{
     e.preventDefault();
     let data = Object.fromEntries(new FormData(e.target));
@@ -21,20 +22,23 @@ formularioPresupuesto.addEventListener("submit", (e)=>{
         });
     }
     tablas();
-    console.log(tipoObjeto);
     formularioPresupuesto.reset();
 });
 
 let tablas = ()=>{
-    document.querySelector('#tablaIngresos').innerHTML = ""
+    document.querySelector('#tablaIngresos').innerHTML = "";
+    let contadorIngreso = 0;
     tipoObjeto.ingresos.forEach((val,id)=>{
         document.querySelector('#tablaIngresos').insertAdjacentHTML("beforeend",`    
             <tr>
                 <td>${val.descripcion}</td> 
                 <td>${val.valor}</td>
             </tr>
-        `)
+        `); 
+        contadorIngreso = contadorIngreso + Number(val.valor);
+        document.querySelector('#ingresosMostrar').textContent = `${contadorIngreso}`
     });
+    let contadorEgresos = 0;
     document.querySelector('#tablaEgresos').innerHTML = ""
     tipoObjeto.egresos.forEach((val,id)=>{
         document.querySelector('#tablaEgresos').insertAdjacentHTML("beforeend",`    
@@ -43,5 +47,14 @@ let tablas = ()=>{
                 <td>${val.valor}</td>
             </tr>
         `)
+        contadorEgresos = contadorEgresos + Number(val.valor);
+        document.querySelector('#egresosMostrar').textContent = `${contadorEgresos}`
     });
+    let disponible = 0;
+    disponible = contadorIngreso - contadorEgresos;
+    document.querySelector('#valorDisponible').textContent = `$ ${disponible}`
+    let porcentaje = (contadorEgresos*100)/contadorIngreso;
+    console.log(porcentaje.toFixed(1));
+    document.querySelector('#porcentajeEgresos').textContent = `${porcentaje} %`
 }
+
