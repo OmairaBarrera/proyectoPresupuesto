d72ef78ecf1caeb169165a0882bc1dc2e2baf323
OmairaBarrera <lomaira.barrera@gmail.com>
2023-04-04 22:44:21 -0500

    fix: :sparkles: Add localstorage in the module
    Use localstorage for the data

--- components/myModulo.js
+++ components/myModulo.js
@@ -1,79 +1,94 @@
-export default{
-    tipoObjeto : {
-    ingresos:[],
-    egresos:[]
-    },
-
+export default{ 
     formulario(){
+        let ingresos=[];
+        if (localStorage.ingresos){
+            ingresos = JSON.parse(localStorage.ingresos);
+        }
+        let egresos=[];
+        if (localStorage.egresos){
+            egresos = JSON.parse(localStorage.egresos);
+        }
         let formularioPresupuesto = document.querySelector("#formularioPresupuesto");
-        var contador = 0,contador2 = 0;
         formularioPresupuesto.addEventListener("submit", (e)=>{
             e.preventDefault();
             let data = Object.fromEntries(new FormData(e.target));
             if(data.tipo == "+"){
-                this.tipoObjeto.ingresos.unshift({
-                tipo: `${data.tipo}`,
-                descripcion: `${data.descripcion}`,
-                valor: `${data.valor}`
+                ingresos.unshift({
+                    tipo: `${data.tipo}`,
+                    descripcion: `${data.descripcion}`,
+                    valor: `${data.valor}`
                 });
-                contador = this.tablaIngresos();
+                localStorage.setItem("ingresos", JSON.stringify(ingresos));
+                this.tablaIngresos();
             } else {
-                this.tipoObjeto.egresos.unshift({
-                tipo: `${data.tipo}`,
-                descripcion: `${data.descripcion}`,
-                valor: `${data.valor}`
+                egresos.unshift({
+                    tipo: `${data.tipo}`,
+                    descripcion: `${data.descripcion}`,
+                    valor: `${data.valor}`
                 });
-                contador2 = this.tablaEgresos(contador);
+                localStorage.setItem("egresos", JSON.stringify(egresos));
+                this.tablaEgresos();
             }
             formularioPresupuesto.reset();
-            this.disponible(contador, contador2);
-            this.porcentajes(contador, contador2);
+            this.disponible();
+            this.porcentajes();
         });
     },
-    
-    disponible(contadorIngreso, contadorEgresos){
-        let disponible = 0;
-        disponible = contadorIngreso - contadorEgresos;
-        document.querySelector('#valorDisponible').textContent = `$ ${disponible}`;
-    },
 
-    tablaIngresos(){
-        document.querySelector('#tablaIngresos').innerHTML = "";
-        var contadorIngreso = 0;
-        this.tipoObjeto.ingresos.forEach((val,id)=>{
-            document.querySelector('#tablaIngresos').insertAdjacentHTML("beforeend",` 
-                <tr>
-                <td>${val.descripcion}</td> 
-                <td>${val.valor}</td>
-            </tr>
-            `); 
+    ingresos(){
+        let data = JSON.parse(localStorage.getItem("ingresos"));
+        let contadorIngreso = 0;
+        data.forEach((val,id)=>{
             contadorIngreso = contadorIngreso + Number(val.valor);
-            document.querySelector('#ingresosMostrar').textContent = `${contadorIngreso}`
         });
         return contadorIngreso;
     },
-    
-    tablaEgresos(contador){
+
+    egresos(){
+        let data = JSON.parse(localStorage.getItem("egresos"));
         let contadorEgresos = 0;
-        document.querySelector('#tablaEgresos').innerHTML = ""
-        this.tipoObjeto.egresos.forEach((val,id)=>{
-            document.querySelector('#tablaEgresos').insertAdjacentHTML("beforeend",` 
-                <tr>
-                <td>${val.descripcion}</td> 
-                <td>${val.valor}</td>
-                <td>${((val.valor*100)/contador).toFixed(1)} % </td>
-                </tr>
-            `)
+        data.forEach((val,id)=>{
             contadorEgresos = contadorEgresos + Number(val.valor);
-            document.querySelector('#egresosMostrar').textContent = `${contadorEgresos}`
         });
         return contadorEgresos;
-    }, 
-    
-    porcentajes(contadorIngreso, contadorEgresos){
-        let porcentaje = 0;
-        porcentaje = (contadorEgresos*100)/contadorIngreso;
-        console.log(porcentaje.toFixed(1));
+    },
+
+    mostrarDatos(){
+        let contadorIngreso = this.ingresos();
+        let contadorEgresos = this.egresos();
+        let disponible = contadorIngreso - contadorEgresos;
+        let porcentaje = (contadorEgresos*100)/contadorIngreso;
+        document.querySelector('#valorDisponible').textContent = `$ ${disponible}`;
+        document.querySelector('#ingresosMostrar').textContent = `${contadorIngreso}`
+        document.querySelector('#egresosMostrar').textContent = `${contadorEgresos}`
         document.querySelector('#porcentajeEgresos').innerHTML = `<span class="badge bg-secondary">${porcentaje.toFixed(1)} %</span>`;
-    }
+    },
+
+    tablaIngresos(){
+        let data = JSON.parse(localStorage.getItem("ingresos"))
+        document.querySelector('#tablaIngresos').innerHTML = "";
+        data.forEach((val,id)=>{
+            document.querySelector('#tablaIngresos').insertAdjacentHTML("beforeend",`    
+                <tr>
+                    <td>${val.descripcion}</td> 
+                    <td>${val.valor}</td>
+                </tr>
+            `); 
+        });
+    },
+
+    tablaEgresos(){
+        let data = JSON.parse(localStorage.getItem("egresos"))
+        let contador = this.ingresos();
+        document.querySelector('#tablaEgresos').innerHTML = ""
+        data.forEach((val,id)=>{
+            document.querySelector('#tablaEgresos').insertAdjacentHTML("beforeend",`    
+                <tr>
+                    <td>${val.descripcion}</td> 
+                    <td>${val.valor}</td>
+                    <td>${((val.valor*100)/contador).toFixed(1)} % </td>
+                </tr>
+            `)
+        })
+    }, 
 }
--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 import myModulo from "./components/myModulo.js";
-		
+
+myModulo.mostrarDatos();
+myModulo.tablaIngresos();
+myModulo.tablaEgresos();
 myModulo.formulario();
+
 
 
